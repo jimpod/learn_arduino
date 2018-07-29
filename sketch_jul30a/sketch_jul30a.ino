06fcdf75f5531e3ac6c519f79fc9b92e7da3c775
Jitkusol Maiket <jitkusol@msn.com>
2018-07-30 02:54:19 +0700

    Add files via upload

---
+++ sketch_jul30a/sketch_jul30a.ino
@@ -0,0 +1,24 @@
+#define LED 13
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED, HIGH);
+  delay(100);
+  digitalWrite(LED, LOW);
+  delay(100);
+  digitalWrite(LED, HIGH);
+  delay(100);
+  digitalWrite(LED, LOW);
+  delay(100);
+  digitalWrite(LED, HIGH);
+  delay(500);
+  digitalWrite(LED, LOW);
+  delay(500);
+
+}
